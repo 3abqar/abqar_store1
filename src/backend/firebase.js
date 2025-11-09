b056238f9d517171ac0b8c6b9c6086f8cc2de7cc
Bishoy <bishoylabib78@gmail.com>
2025-11-09 23:15:20 +0200

    Implement backend for auth

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -1,4 +1,5 @@
-// firebase.js - الكود الكامل والصحيح
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: window.ENV_CONFIG.FIREBASE_API_KEY,
@@ -7,23 +8,30 @@ const firebaseConfig = {
   storageBucket: window.ENV_CONFIG.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: window.ENV_CONFIG.FIREBASE_MESSAGING_SENDER_ID,
   appId: window.ENV_CONFIG.FIREBASE_APP_ID,
-  measurementId: window.ENV_CONFIG.FIREBASE_MEASUREMENT_ID
+  measurementId: window.ENV_CONFIG.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
 
-const salesCollection = db.collection('sales');
-const customersCollection = db.collection('customers');
-const servicesCollection = db.collection('services');
+const salesCollection = db.collection("sales");
+const customersCollection = db.collection("customers");
+const servicesCollection = db.collection("services");
 
 // Export Firebase functions and objects
 window.firebaseExports = {
-  db, auth,
-  salesCollection, customersCollection, servicesCollection,
-  onSnapshot: (ref, callback, errorCallback) => ref.onSnapshot(callback, errorCallback),
+  db,
+  auth,
+  salesCollection,
+  customersCollection,
+  servicesCollection,
+  onSnapshot: (ref, callback, errorCallback) =>
+    ref.onSnapshot(callback, errorCallback),
   addDoc: (collection, data) => collection.add(data),
   doc: (collection, id) => collection.doc(id),
   deleteDoc: (docRef) => docRef.delete(),
@@ -32,7 +40,7 @@ window.firebaseExports = {
   setDoc: (docRef, data) => docRef.set(data),
   query: (collection, ...constraints) => {
     let q = collection;
-    constraints.forEach(constraint => {
+    constraints.forEach((constraint) => {
       if (Array.isArray(constraint) && constraint.length === 3) {
         q = q.where(constraint[0], constraint[1], constraint[2]);
       } else if (Array.isArray(constraint) && constraint.length === 2) {
@@ -48,6 +56,5 @@ window.firebaseExports = {
   serverTimestamp: () => firebase.firestore.FieldValue.serverTimestamp(),
   collection: (path) => db.collection(path),
   onAuthStateChanged: (auth, callback) => auth.onAuthStateChanged(callback),
-  signInAnonymously: (auth) => auth.signInAnonymously()
+  signInAnonymously: (auth) => auth.signInAnonymously(),
 };
-
